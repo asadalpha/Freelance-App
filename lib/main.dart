120384ded6c387f88ff9b063486ef1cc975ab344
Asad <asadalpha77@gmail.com>
2024-01-16 13:13:15 +0530

    adaptive fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:onco_app/UI/pages/login_in_screen.dart';
 import 'package:onco_app/UI/pages/scan_screen.dart';
 import 'package:onco_app/UI/pages/sign_up_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(DevicePreview(
     enabled: !kReleaseMode,
-    builder: (context) => const MyApp(), // Wrap your app
+    builder: (context) => const MyApp(),
   ));
 }
 
@@ -16,12 +18,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      home: const AuthScreen(),
+    //
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+
+          theme: ThemeData(),
+          home: child,
+        );
+      },
+      child: const ScanScreen(),
     );
   }
 }
